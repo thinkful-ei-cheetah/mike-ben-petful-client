6e607a1432b4f79528f88a992303fc411f6c20cf
unknown <kpnba90@gmail.com>
2019-06-21 11:28:04 -0400

    added basic format

---
+++ src/Components/AdoptionPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import ApiService from "../../src/Services/api-services";
+import Dog from "../Components/dog";
+import Cat from '../Components/cat';
+
+export default class AdoptionPage extends React.Component{
+  render() {
+    return (
+      <section>
+        <h1>Adoption Page</h1>
+        <div className='pets-info'>
+          
+        </div>
+      </section>
+    )
+  }
+}
---
+++ src/Components/dog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Dog extends React.Component{
+  render() {
+    return(
+      <section>
+        <div className='dogs-info'>
+          
+        </div>
+      </section>
+    )
+  }
+}
